cc78369a475ac9b00ca0c82bf864bf1fce9a283e
knowthehiddenone-creator <knowthehiddenone@gmail.com>
2026-06-14 21:26:06 +0530

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/StructAIBasePlate">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
